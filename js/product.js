d56ac181937fcc47a4e54411709de47caddb7903
KristofferRottingen <kriroe06@gmail.com>
2021-03-19 21:19:42 +0100

    java added

--- js/product.js
+++ js/product.js
@@ -4,14 +4,26 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("i");
+const heading = params.get("h");
 const image = document.querySelector(".jacket-img");
-const containerColor = document.querySelector("#background-jackets-product");
+const background = document.querySelector("#background-jackets-product");
+const containerColor = document.querySelector("#text-container-product");
+const productHeading = document.querySelector(".left-text h1");
 
 console.log(id);
 console.log(image);
 
 if (id === "hiking-men") {
-    image.innerHTML = `<img src="../images/CAMPING DAME.png" alt="Jacket women Camping" />`;
-    containerColor.classList.add("background-jackets-camping");
-}
+    image.innerHTML = `<img src="../images/HIKING HERRE test.png"
+    alt="Jacket man Hiking" />`;
+    background.classList.add("background-jackets");
+    containerColor.classList.add("text-container");
+    productHeading.innerHTML = `${heading}`;
+} else if (id === "hiking-women") {
+    image.innerHTML = `<img src="images/HIKING.png" alt="Hiking Dame jakke"
+    />`;
+    background.classList.add("background-jackets");
+    containerColor.classList.add("text-container-hiking-women");
+    productHeading.innerHTML = `${heading}`;
+} 
 
