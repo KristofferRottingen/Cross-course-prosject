7b6acbd4a1e756c30430cc3c83155eac0fcaf900
KristofferRottingen <kriroe06@gmail.com>
2021-03-19 20:37:17 +0100

    Update product.js

--- js/product.js
+++ js/product.js
@@ -9,6 +9,7 @@ console.log(id);
 
 const image = document.querySelector("jacket-img");
 
+console.log(image);
 
 if (id === "hiking-men") {
     image.style.backgroundImage = "url(../../images/CAMPING.png)";
