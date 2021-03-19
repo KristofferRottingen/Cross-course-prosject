1373465d063e253fcb160c0789d6e8378dcfb407
KristofferRottingen <kriroe06@gmail.com>
2021-03-19 20:38:52 +0100

    Update product.js

--- js/product.js
+++ js/product.js
@@ -4,11 +4,9 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("i");
+const image = document.querySelector(".jacket-img");
 
 console.log(id);
-
-const image = document.querySelector("jacket-img");
-
 console.log(image);
 
 if (id === "hiking-men") {
