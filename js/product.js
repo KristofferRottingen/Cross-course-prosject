28fa338bfc6335d8426224904da2065f140094f7
KristofferRottingen <kriroe06@gmail.com>
2021-03-19 20:55:20 +0100

    nice

--- js/product.js
+++ js/product.js
@@ -5,10 +5,13 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("i");
 const image = document.querySelector(".jacket-img");
+const containerColor = document.querySelector("#background-jackets-product");
 
 console.log(id);
 console.log(image);
 
 if (id === "hiking-men") {
-    image.innerHTML = `<img src="../images/CAMPING DAME.png" alt="Jacket women Camping" />`
+    image.innerHTML = `<img src="../images/CAMPING DAME.png" alt="Jacket women Camping" />`;
+    containerColor.classList.add("background-jackets-camping");
 }
+
